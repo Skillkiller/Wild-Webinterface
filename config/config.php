89a991ea353c33c94f5df85c7c102a238d960cea
Skillkiller <Skillkiller@thecoin.de>
2017-03-27 21:09:12 +0200

    Config Angelegt
    Die Config wurde erstellt damit nicht in jeder .php Datei die
    Anmeldedaten neu definiert werden müssen

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+// die Konstanten auslagern in eigene Datei
+// geladen wird.
+ 
+// Damit alle Fehler angezeigt werden
+error_reporting(E_ALL);
+ 
+// Zum Aufbau der Verbindung zur Datenbank
+// die Daten erhalten Sie von Ihrem Provider
+define ( 'MYSQL_HOST',      'localhost' );
+ 
+// bei XAMPP ist der MYSQL_Benutzer: root
+define ( 'MYSQL_BENUTZER',  '' );
+define ( 'MYSQL_KENNWORT',  '' );
+// für unser Bsp. nennen wir die DB adressverwaltung
+?>
